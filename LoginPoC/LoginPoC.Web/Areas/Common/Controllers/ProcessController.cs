90cdc228514d9ad2beb41de52953ae029f8791a5
Rafael <rafaga_16@hotmail.com>
2017-09-15 10:18:06 -0300

    Nuevos Controllers para vistas + Avance en creacion de Tipo de Tramites

---
+++ LoginPoC/LoginPoC.Web/Areas/Common/Controllers/ProcessController.cs
@@ -0,0 +1,36 @@
+﻿using LoginPoC.DAL;
+using LoginPoC.Model.ProcessType;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using LoginPoC.Model.User;
+using LoginPoC.Core.ProcessType;
+
+namespace LoginPoC.Web.Areas.Common.Controllers
+{
+    [Authorize(Roles = ApplicationUserRoles.Agent + ", " + ApplicationUserRoles.Administrator)]
+    public class ProcessController : Controller
+    {
+        public ProcessController()
+        {
+            
+        }
+
+        // GET: ProcessType
+        [OverrideAuthorization]
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ProcessType
+        [OverrideAuthorization]
+        [Authorize]
+        public ActionResult MyProcesses()
+        {
+            return View();
+        }
+    }
+}
