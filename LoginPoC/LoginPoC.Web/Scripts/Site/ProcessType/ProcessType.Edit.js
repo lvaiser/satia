702398f32022515d4c4ec22ea646e75c3ffb3fa9
Romina Riedel <riedelromina@gmail.com>
2017-09-15 20:25:59 -0300

    Agrego controller para la edicion de tipos de tramite

---
+++ LoginPoC/LoginPoC.Web/Scripts/Site/ProcessType/ProcessType.Edit.js
@@ -0,0 +1,34 @@
+﻿(function () {
+
+    app.controller("ProcessType.Edit", ["$scope", "$http", controller]);
+
+    function controller($scope, $http) {
+        $scope.processTypeFields = [];
+        // TODO request field types by http
+        $scope.fieldTypes = ['integer', 'date', 'phoneNumber', 'text'];
+ 
+        const emptyProcessTypeField = {
+            name: null,
+            type: null,
+            isRequired: false,
+            fieldType: $scope.fieldTypes[0]
+        }
+
+        $scope.events = {
+            onInit: onInit,
+            onSaveClicked: onSaveClicked
+        };
+
+        function onInit(processType) {
+            $scope.processType = processType;
+        }
+
+        function onSaveClicked() {
+        }
+
+        function newField() {
+            $scope.processTypeFields = $scope.processTypeFields.concat(emtpyProccesTypeField);
+        }
+    }
+
+})();
