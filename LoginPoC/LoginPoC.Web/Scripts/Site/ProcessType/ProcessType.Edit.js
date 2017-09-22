9659215df536e5f3a4e099df019b2f72d649a93e
Romina Riedel <riedelromina@gmail.com>
2017-09-21 21:18:43 -0300

    Sacar campos, agregar y sacar documentos del tipo de documento

--- LoginPoC/LoginPoC.Web/Scripts/Site/ProcessType/ProcessType.Edit.js
+++ LoginPoC/LoginPoC.Web/Scripts/Site/ProcessType/ProcessType.Edit.js
@@ -4,6 +4,7 @@
 
     function controller($scope, $http) {
         $scope.processTypeFields = [];
+        $scope.processTypeDocuments = [];
         // TODO request field types by http
         $scope.fieldTypes = [
             { id: 1, name: 'integer' },
@@ -19,9 +20,17 @@
             fieldType: $scope.fieldTypes[0]
         }
 
+        $scope.emptyDocument = {
+            name: null,
+            isRequired: false
+        }
+
         $scope.events = {
             onInit: onInit,
             newField: newField,
+            newDocument: newDocument,
+            deleteDocument: deleteDocument,
+            deleteField: deleteField,
             onSaveClicked: onSaveClicked
         };
 
@@ -30,10 +39,32 @@
         }
 
         function onSaveClicked() {
+            $scope.processType.documents = $scope.processTypeDocuments;
+            $scope.processType.fields = $scope.processTypeFields;
+            return $http.post("/Admin/ProcessTypes/Edit", $scope.processType)
+                .then(function (response) {
+                    $.notify("Los datos se actualizaron exitosamente", "success");
+                }, Utils.onAjaxError.bind(this, " al guardar el tipo de documento"));
         }
 
         function newField() {
             $scope.processTypeFields = $scope.processTypeFields.concat(angular.copy($scope.emptyProcessTypeField));
+        }
+
+        function newDocument() {
+            $scope.processTypeDocuments = $scope.processTypeDocuments.concat(angular.copy($scope.emptyProcessTypeDocument));
+        }
+
+        function deleteField(field) {
+            deleteItemFromArr($scope.processTypeFields, field);
+        }
+
+        function deleteDocument(document) {
+            deleteItemFromArr($scope.processTypeDocuments, document);
+        }
+
+        function deleteItemFromArr(arr, item) {
+            arr.splice(arr.indexOf(item), 1);
         }
     }
 
