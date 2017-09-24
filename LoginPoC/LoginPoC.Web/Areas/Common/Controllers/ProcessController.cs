65bc418286346d1f7bddd7964d60ff2d253c4f80
Leandro Galluppi <galuv2@gmail.com>
2017-09-24 19:08:52 -0300

    Mocks de las pantallas de tramites guardados, tramites y edicion/visualizacion/creacion de tramite

--- LoginPoC/LoginPoC.Web/Areas/Common/Controllers/ProcessController.cs
+++ LoginPoC/LoginPoC.Web/Areas/Common/Controllers/ProcessController.cs
@@ -6,6 +6,11 @@ using System.Net;
 using System.Web.Mvc;
 using LoginPoC.Model.User;
 using LoginPoC.Core.ProcessType;
+using LoginPoC.Web.Areas.Admin.Models;
+using System.Collections;
+using System.Collections.Generic;
+using LoginPoC.Web.Areas.Common.Models;
+using System.Dynamic;
 
 namespace LoginPoC.Web.Areas.Common.Controllers
 {
@@ -13,24 +18,64 @@ namespace LoginPoC.Web.Areas.Common.Controllers
     public class ProcessController : Controller
     {
         public ProcessController()
+        {  }
+
+        // GET: ProcessType
+        [Authorize]
+        public ActionResult Index(string name = null)
         {
-            
+            //var process = await this.ProcessService.SearchAsync(name);
+            var process = this.GetMockedProcesses();
+            var vm = new ProcessIndexViewModel()
+            {
+                Processes = process,
+                SearchByName = name
+            };
+
+            return View(vm);
         }
 
         // GET: ProcessType
         [OverrideAuthorization]
         [Authorize]
-        public ActionResult Index()
+        public ActionResult MyProcesses(string name = null)
+        {
+            //var process = await this.ProcessService.SearchMyProcessesAsync(name);
+            var process = this.GetMockedProcesses();
+            var vm = new ProcessIndexViewModel()
+            {
+                Processes = process,
+                SearchByName = name
+            };
+
+            return View(vm);
+        }
+
+        [OverrideAuthorization]
+        [Authorize]
+        public ActionResult Edit(int Id)
         {
             return View();
         }
 
-        // GET: ProcessType
-        [OverrideAuthorization]
-        [Authorize]
-        public ActionResult MyProcesses()
+        private IEnumerable<dynamic> GetMockedProcesses()
         {
-            return View();
+            dynamic uno = new ExpandoObject();
+            uno.Id = 1;
+            uno.Name = "Residencia";
+            uno.Status = "En Proceso";
+
+            dynamic dos = new ExpandoObject();
+            dos.Id = 1;
+            dos.Name = "Ciudadania";
+            dos.Status = "Finalizado";
+
+            dynamic tres = new ExpandoObject();
+            tres.Id = 1;
+            tres.Name = "Tramite X";
+            tres.Status = "Rechazado";
+
+            return new List<dynamic>() { uno, dos, tres };
         }
     }
 }
