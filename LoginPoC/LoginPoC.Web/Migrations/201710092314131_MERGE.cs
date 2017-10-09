19f63684a63ef4846458b477619446b01aa3ae80
lvaiser <leanrv@gmail.com>
2017-10-09 20:43:04 -0300

    Mejoras en instrucciones de resolución de conflictos en migrations

--- LoginPoC/LoginPoC.Web/Migrations/201710092314131_MERGE.cs
+++ LoginPoC/LoginPoC.Web/Migrations/201710092314131_MERGE.cs
@@ -10,7 +10,7 @@ namespace LoginPoC.Web.Migrations
             // Se borró el contenido a propósito debido a que la última migration se creó sin correr la anterior.
 
             // Si vuelve a pasar, SOLUCION:
-            // 1) Add -Migration MERGE 
+            // 1) Abrir la Package Manager Console y ejecutar: Add-Migration MERGE
             // 2) Abrir el archivo <TIMESTAMP>_MERGE.cs y comentar todo el código duplicado de migraciones anteriores.
 
             // explicación en: https://stackoverflow.com/questions/17921886/update-database-fails-due-to-pending-changes-but-add-migration-creates-a-duplic?rq=1
