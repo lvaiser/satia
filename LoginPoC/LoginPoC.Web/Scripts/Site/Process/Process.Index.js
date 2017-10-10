1ca6aaf0a184340a4669698d8bbc05ebee34b63c
Romina Riedel <riedelromina@gmail.com>
2017-10-09 23:47:00 -0300

    Se selecciona tipo de documento cuando se crea un documento

---
+++ LoginPoC/LoginPoC.Web/Scripts/Site/Process/Process.Index.js
@@ -0,0 +1,17 @@
+﻿(function () {
+
+    app.controller("Process.Index", ["$scope", "$http", controller]);
+
+    function controller($scope, $http) {
+        $scope.events = {
+            onInit: onInit
+        };
+
+        function onInit(model) {
+            $scope.processes = model.processes;
+            $scope.processTypes = model.processTypes;
+        }
+        
+    }
+
+})();
