ca48030d1fe3c2073b6f0ae76b78a5b5a60a3e02
Rafael <rafaga_16@hotmail.com>
2017-11-11 18:51:17 -0300

    Ediciones de textos y estilos

--- LoginPoC/LoginPoC.Web/Areas/Security/Models/AccountViewModels.cs
+++ LoginPoC/LoginPoC.Web/Areas/Security/Models/AccountViewModels.cs
@@ -120,7 +120,7 @@ namespace LoginPoC.Web.Areas.Security.Models
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare("Password", ErrorMessage = "La contraseña y la confirmación no coinciden.")]
         public string ConfirmPassword { get; set; }
     }
 
@@ -132,14 +132,14 @@ namespace LoginPoC.Web.Areas.Security.Models
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "La {0} debe tener al menos {2} caracteres.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Contraseña")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Confirmar Contraseña")]
+        [Compare("Password", ErrorMessage = "La contraseña y la confirmación no coinciden.")]
         public string ConfirmPassword { get; set; }
 
         public string Code { get; set; }
