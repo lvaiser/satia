526fcfac2e430002dccf6ed08ec2edba8438a4c6
Rafael <rafaga_16@hotmail.com>
2017-11-11 19:38:11 -0300

    Edicion de Checkbox

--- LoginPoC/LoginPoC.Web/Areas/Security/Models/AccountViewModels.cs
+++ LoginPoC/LoginPoC.Web/Areas/Security/Models/AccountViewModels.cs
@@ -113,7 +113,7 @@ namespace LoginPoC.Web.Areas.Security.Models
 	public class RegisterViewModel : ExternalLoginConfirmationViewModel
 	{
 		[Required]
-		[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+		[StringLength(100, ErrorMessage = "La contraseña debe tener al menos 6 caracteres.", MinimumLength = 6)]
 		[DataType(DataType.Password)]
 		[Display(Name = "Password")]
 		public string Password { get; set; }
@@ -132,7 +132,7 @@ namespace LoginPoC.Web.Areas.Security.Models
 		public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "La {0} debe tener al menos {2} caracteres.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "La contraseña debe tener al menos 6 caracteres.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Contraseña")]
         public string Password { get; set; }
